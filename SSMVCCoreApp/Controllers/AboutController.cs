e4ed67e1051ce8bfb3249e5c6284bb579b800504
Rohit Kulkarni <rht_kulkarni@outlook.com>
2020-06-17 12:09:46 +0000

    First Commit

---
+++ SSMVCCoreApp/Controllers/AboutController.cs
@@ -0,0 +1,41 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SSMVCCoreApp.Controllers
+{
+    public class AboutController : Controller
+    {
+        private readonly IConfiguration _configuration;
+        public AboutController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            AzureServiceTokenProvider azureServiceTokenProvider = new AzureServiceTokenProvider();
+            KeyVaultClient keyVaultClient = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
+
+            var secrets = await keyVaultClient.GetSecretsAsync($"{ _configuration["rksskeyvault"]}");
+
+            Dictionary<string, string> secretValueList = new Dictionary<string, string>();
+            foreach (var item in secrets)
+            {
+                var secret = await keyVaultClient.GetSecretAsync($"{item.Id}");
+                secretValueList.Add(item.Id,secret.Value);
+            }
+
+            return View(secretValueList);
+        }
+        public IActionResult Throw()
+        {
+            throw new EntryPointNotFoundException("This is a user thrown exception");
+        }
+    }
+}
---
+++ SSMVCCoreApp/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureKeyVault;
+using Microsoft.Extensions.Logging;
+
+namespace SSMVCCoreApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    if (!hostingContext.HostingEnvironment.IsDevelopment())
+                    {
+                        SetupKeyVault(hostingContext, config);
+                    }
+                    else if (hostingContext.HostingEnvironment.IsDevelopment())
+                    {
+                        SetupKeyVault(hostingContext, config);
+                    }
+                })
+                .UseStartup<Startup>();
+
+        private static void SetupKeyVault(WebHostBuilderContext hostingContext, IConfigurationBuilder config)
+        {
+            var buildConfig = config.Build();
+            var keyVaultEndpoint = buildConfig["rksskeyvault"];
+
+            if (!string.IsNullOrEmpty(keyVaultEndpoint))
+            {
+                var azureServiceTokeProvider = new AzureServiceTokenProvider();
+                var keyVaultClient = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(azureServiceTokeProvider.KeyVaultTokenCallback));
+                config.AddAzureKeyVault(keyVaultEndpoint, keyVaultClient, new DefaultKeyVaultSecretManager());
+            }
+        }
+    }
+}
